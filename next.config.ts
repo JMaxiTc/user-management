4fd192fa6929a48c6405b23598911d47b87fc301
JMaxiTc <jesumax12@hotmail.com>
2025-04-06 23:59:35 -0600

    v.0.5

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+}
+  
+  module.exports = nextConfig
+
+
+/*import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here
 };
 
-export default nextConfig;
+export default nextConfig;*/
